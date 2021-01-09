b48144b95bc1b5bf95e30a6ff3b9382bd5a96dd5
AGUSZ12 <77011008+AGUSZ12@users.noreply.github.com>
2021-01-09 18:42:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
-            + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
-            + 'END:VCARD'
+const  BotName  =  'APBOT';
+const     instagram     =     'axfc_ap';
+const       aktif       =       'Kapan bot aktif' ;
+const   vcard   =   'BEGIN: VCARD \ n'   // metadata dari kartu kontak
+            +     'VERSI: 3.0 \ n' 
+            +        'FN: Agus \ n'        // Nama kamu
+            +            'ORG: VVIPINDO; \ n '            // Nama bot
+            +                       'TEL; ketik = SEL; ketik = SUARA; waid = 62896943543843: +62896943543849 \ n '                       // Nomor whatsapp kamu
+            +                'SELESAI: VCARD'
 const
 {
 WAConnection,
